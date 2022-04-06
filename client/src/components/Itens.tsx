9f77c0000f62d9f7968cbd940f1d6ac0e974e4d1
rafa <jerhemoso18@gmail.com>
2022-04-05 22:11:31 -0300

    implementado busca na home por titulo do evento

--- client/src/components/Itens.tsx
+++ client/src/components/Itens.tsx
@@ -1,19 +1,24 @@
-import Card from '@mui/material/Card';
-import {
-  Box, Button, ThemeProvider, Typography,
-} from '@mui/material';
-import '../styles/Itens.css';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Grid from '@mui/material/Grid';
-import React from 'react';
-import ButtonStyles from '../styles/MuiStyles';
-import img1 from '../images/e3.jpeg';
-import img2 from '../images/f8.png';
-import img3 from '../images/gio.jpg';
-import img4 from '../images/tga.jpg';
-import ModalDetails from './ModalDetails';
+import Card from "@mui/material/Card";
+import { Box, Button, ThemeProvider, Typography } from "@mui/material";
+import "../styles/Itens.css";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import React from "react";
+import ButtonStyles from "../styles/MuiStyles";
+import img1 from "../images/e3.jpeg";
+import img2 from "../images/f8.png";
+import img3 from "../images/gio.jpg";
+import img4 from "../images/tga.jpg";
+import ModalDetails from "./ModalDetails";
+import { CommitSharp } from "@mui/icons-material";
+
+export interface Props {
+  searchValues: string;
+
+  // handleclick
+}
 
 interface EventData {
   title: string;
@@ -23,33 +28,53 @@ interface EventData {
   remainingVacancies: number;
 }
 
-const Itens: React.FC = () => {
+const Itens: React.FC<Props> = ({ searchValues }) => {
   const [openModalDetails, setOpenModalDetails] = React.useState(false);
   const [itensData, setItensData] = React.useState<EventData>({
-    title: '',
-    description: '',
-    img: '',
+    title: "",
+    description: "",
+    img: "",
     remainingVacancies: 0,
   });
+
   const theme = ButtonStyles;
   const itens = [
     {
-      title: 'E3',
-      description: 'A Electronic Entertainment Expo, mais conhecida como E3, é uma feira internacional dedicada a jogos eletrônicos.',
+      title: "E3",
+      description:
+        "A Electronic Entertainment Expo, mais conhecida como E3, é uma feira internacional dedicada a jogos eletrônicos.",
       img: img1,
-      value: 0.00,
+      value: 0.0,
       remainingVacancies: 4,
     },
     {
-      title: 'Titulo 2', description: 'Descrição 2', img: img2, value: 0.00, remainingVacancies: 14,
+      title: "Titulo 2",
+      description: "Descrição 2",
+      img: img2,
+      value: 0.0,
+      remainingVacancies: 14,
     },
     {
-      title: 'Titulo 3', description: 'Descrição 3', img: img3, value: 0.00, remainingVacancies: 2,
+      title: "Titulo 3",
+      description: "Descrição 3",
+      img: img3,
+      value: 0.0,
+      remainingVacancies: 2,
     },
     {
-      title: 'Titulo 4', description: 'Descrição 4', img: img4, value: 0.00, remainingVacancies: 12,
+      title: "Titulo 4",
+      description: "Descrição 4",
+      img: img4,
+      value: 0.0,
+      remainingVacancies: 12,
     },
   ];
+
+  const filteredItens = !!searchValues
+    ? itens.filter((item: any) => {
+        return item.title.toLowerCase().includes(searchValues.toLowerCase());
+      })
+    : itens;
 
   const HandleOpenModalDetails = (obj: EventData) => {
     setOpenModalDetails(true);
@@ -57,29 +82,52 @@ const Itens: React.FC = () => {
   };
   return (
     <ThemeProvider theme={theme}>
-      <Box className='fullBody' >
-        <Box className='itensBody'>
-          <Grid container rowSpacing={'50px'} columnSpacing={2} columns={{ xs: 2, sm: 4, md: 6 }}>
-            {itens.map((item) => (
-              <Grid item xs={2} sm={4} md={3} >
-                <Card sx={{ background: '#1C1B1F' }}>
+      <Box className="fullBody">
+        <Box className="itensBody">
+          <Grid
+            container
+            rowSpacing={"50px"}
+            columnSpacing={2}
+            columns={{ xs: 2, sm: 4, md: 6 }}
+          >
+            {filteredItens.map((item) => (
+              <Grid item xs={2} sm={4} md={3}>
+                <Card sx={{ background: "#1C1B1F" }}>
                   <CardMedia
                     component="img"
                     height="400"
                     image={item.img}
-                  // alt="green iguana"
+                    // alt="green iguana"
                   />
                   <CardContent>
-                    <Typography gutterBottom variant="h5" component="div" sx={{ color: '#E6E1E5' }}>
+                    <Typography
+                      gutterBottom
+                      variant="h5"
+                      component="div"
+                      sx={{ color: "#E6E1E5" }}
+                    >
                       {item.title}
                     </Typography>
-                    <Typography variant="body2" color="text.secondary" sx={{ color: '#E6E1E5' }}>
+                    <Typography
+                      variant="body2"
+                      color="text.secondary"
+                      sx={{ color: "#E6E1E5" }}
+                    >
                       {item.description}
                     </Typography>
                   </CardContent>
-                  <CardActions sx={{ justifyContent: 'flex-end' }}>
-                    <Button size="small" variant='contained' color='secondary'>Compartilhar</Button>
-                    <Button size="small" variant='contained' color='secondary' onClick={() => HandleOpenModalDetails(item)}>Veja mais</Button>
+                  <CardActions sx={{ justifyContent: "flex-end" }}>
+                    <Button size="small" variant="contained" color="secondary">
+                      Compartilhar
+                    </Button>
+                    <Button
+                      size="small"
+                      variant="contained"
+                      color="secondary"
+                      onClick={() => HandleOpenModalDetails(item)}
+                    >
+                      Veja mais
+                    </Button>
                   </CardActions>
                 </Card>
               </Grid>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,57 +1,69 @@
-import '../styles/Home.css';
-import { ThemeProvider } from '@mui/material/styles';
-import { Box } from '@material-ui/core';
-import { IconButton, InputAdornment } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import ClearIcon from '@mui/icons-material/Clear';
-import { useState } from 'react';
-import Header from '../components/Header';
-import ResponsiveDrawer from '../components/ResponsiveDrawer';
-import Itens from '../components/Itens';
-import ButtonStyles from '../styles/MuiStyles';
-import SearchField from '../components/SearchField';
+import "../styles/Home.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { Box } from "@material-ui/core";
+import { IconButton, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from "@mui/icons-material/Clear";
+import { useState } from "react";
+import Header from "../components/Header";
+import ResponsiveDrawer from "../components/ResponsiveDrawer";
+import Itens from "../components/Itens";
+import ButtonStyles from "../styles/MuiStyles";
+import SearchField from "../components/SearchField";
 
 function Home() {
   const theme = ButtonStyles;
   const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+  const [searchValues, setSearchValues] = useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(event.target.value);
+    setSearchValues(event.target.value);
+  };
+
   const pages = [
-    { id: 0, text: 'Sobre nós' },
-    { id: 1, text: 'FAQ' },
-    { id: 2, text: 'Contato' },
+    { id: 0, text: "Sobre nós" },
+    { id: 1, text: "FAQ" },
+    { id: 2, text: "Contato" },
   ];
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{
-        width: '100%',
-      }}>
+      <Box
+        sx={{
+          width: "100%",
+        }}
+      >
         <Header
           pages={pages}
           isOpenDrawer={isOpenDrawer}
-          setIsOpenDrawer={setIsOpenDrawer} />
+          setIsOpenDrawer={setIsOpenDrawer}
+        />
 
         <ResponsiveDrawer
           pages={pages}
           isOpenDrawer={isOpenDrawer}
-          setIsOpenDrawer={setIsOpenDrawer} />
-        <Box
-          className='divSearchbarAndItens'
-        >
-          <SearchField sx={{ marginTop: { sm: '35px' } }} fullWidth placeholder='Pesquisar evento ou apresentação'
+          setIsOpenDrawer={setIsOpenDrawer}
+        />
+        <Box className="divSearchbarAndItens">
+          <SearchField
+            onChange={handleChange}
+            sx={{ marginTop: { sm: "35px" } }}
+            fullWidth
+            placeholder="Pesquisar evento ou apresentação"
             InputProps={{
               startAdornment: (
                 <InputAdornment position="start">
-                  <SearchIcon color='secondary' />
+                  <SearchIcon color="secondary" />
                 </InputAdornment>
               ),
               endAdornment: (
                 <IconButton>
-                  <ClearIcon color='secondary' />
+                  <ClearIcon color="secondary" />
                 </IconButton>
               ),
             }}
           />
           <Box>
-            <Itens/>
+            <Itens searchValues={searchValues} />
           </Box>
         </Box>
       </Box>
