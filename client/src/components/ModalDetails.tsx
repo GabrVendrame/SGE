b4317cfb7556cc5c292d590c3754b6c81e5e9610
stanyzra <stanyhelberth@gmail.com>
2022-04-08 22:54:02 -0300

    modal de detalhes implementado. Proximo passo é jogar os eventos e apresentações pro backend e fazer a tela de compra de ingresso após isso

--- client/src/components/ModalDetails.tsx
+++ client/src/components/ModalDetails.tsx
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import ScrollContainer from 'react-indiana-drag-scroll';
 import {
+  Button,
   Card, CardActionArea, CardContent, Divider, Grid, ThemeProvider,
 } from '@mui/material';
 import MuiStyles from '../styles/MuiStyles';
@@ -57,49 +58,55 @@ const ModalDetails: React.FC<Props> = ({
     // dateByDay.map((days) => {
     //   console.log(days);
     // });
-    // return (<Box>teste</Box>);
     return (
       <ThemeProvider theme={theme}>
-        <Card className='dayCard' sx={{ background: '#1C1B1F', display: 'flex', overflow: 'initial' }} >
-          <CardActionArea
-            onClick={(e) => handleCardClick(e)}>
-            <CardContent>
-              <Typography gutterBottom component="div" color="secondary" sx={{ minWidth: '165px' }}>
-                Dia - {dateByDay.finalDate.getDate()}/{dateByDay.finalDate.getMonth()}
-              </Typography>
-              <Typography gutterBottom component="div" color="primary">
-                Horário início - {dateByDay.initialDate.getHours()}
-                :
-                {dateByDay.initialDate.getMinutes()}
-              </Typography>
-              <Typography gutterBottom component="div" color="primary">
-                Horário término - {dateByDay.finalDate.getHours()}
-                :
-                {dateByDay.finalDate.getMinutes()}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+        <CardContent>
+          <Typography gutterBottom component="div" color="secondary" sx={{ minWidth: '165px' }}>
+            Dia - {dateByDay.finalDate.getDate()}/{dateByDay.finalDate.getMonth()}
+          </Typography>
+          <Typography gutterBottom component="div" color="primary">
+            Horário início - {dateByDay.initialDate.getHours()}
+            :
+            {dateByDay.initialDate.getMinutes()}
+          </Typography>
+          <Typography gutterBottom component="div" color="primary">
+            Horário término - {dateByDay.finalDate.getHours()}
+            :
+            {dateByDay.finalDate.getMinutes()}
+          </Typography>
+        </CardContent>
       </ThemeProvider>
     );
   };
 
   const PresentationDetailsAfterClick: React.FC = () => {
     return (
-      <Box>
-        <Typography color='secondary' sx={{ mt: 2, mb: 2 }}>
-          Descrição da apresentação
-        </Typography>
-        <Typography variant='body2' color={'rgba(255, 255, 255, 0.7)'}>
-          {presentationData.description}
-        </Typography>
-        <Typography color='secondary' sx={{ mt: 2, mb: 2 }}>
-          Agenda da apresentação
-        </Typography>
-        {presentationData.dateByDay.map((schedules) => (
-          <ScheduleCard dateByDay={schedules} />
-        ))}
-      </Box>
+      <ThemeProvider theme={theme}>
+
+        <ScrollContainer className='presentationDetails'>
+
+          <Typography color='secondary' sx={{ mb: 2 }} >
+            Descrição da apresentação
+          </Typography>
+          <Typography variant='body2' color={'rgba(255, 255, 255, 0.7)'}>
+            {presentationData.description}
+          </Typography>
+          <Typography color='secondary' sx={{ mt: 2, mb: 2 }}>
+            Agenda da apresentação
+          </Typography>
+          {presentationData.dateByDay.map((schedules) => (
+            <Card className='dayCard' sx={{
+              background: '#1C1B1F', display: 'flex', overflow: 'initial', minWidth: 'fit-content',
+            }} >
+              <ScheduleCard dateByDay={schedules} />
+            </Card>
+          ))}
+        </ScrollContainer>
+        <Box className='buttonComprarIngresso'>
+
+          <Button>Comprar ingresso</Button>
+        </Box>
+      </ThemeProvider>
     );
   };
 
@@ -136,19 +143,32 @@ const ModalDetails: React.FC<Props> = ({
                 </Box>
               </Grid>
               <Grid item sm={8} md={4}>
-                <Box>
-                  <Typography color='secondary' sx={{ mt: 2, mb: 2 }}>
-                    Agenda do evento
-                  </Typography>
-                  <ScrollContainer className="eventsSchedule">
-                    {eventData.dateByDay.map((schedules) => (
-                      <ScheduleCard dateByDay={schedules} />
-                    ))}
-                  </ScrollContainer>
+                <Box className='rightGridContainer'>
+                  <Box sx={{ mb: '23px' }}>
+                    <Typography color='secondary' sx={{ mt: 2, mb: 2 }}>
+                      Agenda do evento
+                    </Typography>
+                    <ScrollContainer className="eventsSchedule">
+                      {eventData.dateByDay.map((schedules) => (
+                        <Card sx={{
+                          background: '#1C1B1F',
+                          display: 'flex',
+                          overflow: 'initial',
+                          minWidth: 'fit-content',
+                        }} >
+                          <CardActionArea
+                            onClick={(e) => handleCardClick(e)}>
+                            <ScheduleCard dateByDay={schedules} />
+                          </CardActionArea>
+                        </Card>
+                      ))}
+                    </ScrollContainer>
+                  </Box>
                   {selectedPresentation
                     ? <PresentationDetailsAfterClick />
                     : null}
                 </Box>
+
               </Grid>
             </Grid>
           </Box>
