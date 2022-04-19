ec99e85d90139bd8f5035df39465c7682a09dc62
rafa <jerhemoso18@gmail.com>
2022-04-19 19:37:52 -0300

    esboço da pagina de usuario comum e logout

---
+++ client/src/components/Header2.tsx
@@ -0,0 +1,104 @@
+import "../styles/Header.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { AppBar, Button, IconButton, Toolbar } from "@mui/material";
+import Box from "@mui/material/Box/Box";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import React from "react";
+import MuiStyles from "../styles/MuiStyles";
+import logo from "../images/logo.png";
+
+// const pages = ['Sobre nós', 'FAQ', 'Contato'];
+interface Props {
+  isOpenDrawer: boolean;
+  setIsOpenDrawer: React.Dispatch<React.SetStateAction<boolean>>;
+  pages: { id: number; text: string }[];
+}
+
+const Header2: React.FC<Props> = ({ isOpenDrawer, setIsOpenDrawer, pages }) => {
+  const theme = MuiStyles;
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    console.log("saindo");
+    alert("saindo");
+    navigate("/");
+  };
+
+  const handleOpenNavMenu = () => {
+    setIsOpenDrawer(!isOpenDrawer);
+  };
+
+  const handlePageSelect = (id: number) => {
+    // setAnchorElNav(null);
+    console.log(id);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <AppBar position="static">
+        {/* <Container className='containerHeader'> */}
+        <Toolbar disableGutters>
+          <Box
+            className="logoAndName"
+            sx={{ display: { xs: "none", sm: "flex" } }}
+          >
+            <Link to="/">
+              <img src={logo} alt="S-logo" className="logo" />
+            </Link>
+            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+              <Box className="projectName">SOGEI Project</Box>
+            </Link>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", sm: "none" } }}>
+            <IconButton
+              size="small"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", sm: "none" } }}>
+            <Link to="/">
+              <img src={logo} alt="S-logo" className="logo" />
+            </Link>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.id}
+                onClick={() => handlePageSelect(page.id)}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page.text}
+              </Button>
+            ))}
+          </Box>
+
+          <Button
+            onClick={() => logout()}
+            color="secondary"
+            variant="contained"
+            sx={{
+              display: { xs: "none", sm: "flex" },
+              position: "absolute",
+              right: "10px",
+              top: 15,
+            }}
+          >
+            Sair
+          </Button>
+        </Toolbar>
+        {/* </Container> */}
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header2;
--- client/src/pages/HomePageUser.tsx
+++ client/src/pages/HomePageUser.tsx
@@ -2,57 +2,111 @@ import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Home.css";
 import Axios from "axios";
-export interface user {
+import "../styles/Home.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { Box } from "@material-ui/core";
+import { IconButton, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from "@mui/icons-material/Clear";
+import Header from "../components/Header2";
+import ResponsiveDrawer from "../components/ResponsiveDrawer";
+import Itens from "../components/Itens";
+import ButtonStyles from "../styles/MuiStyles";
+import SearchField from "../components/SearchField";
+
+type user = {
   name: string;
   email: string;
   password: string;
   cpfCnpj: string;
   cell: string;
   userType: string;
-}
+};
+
+type obj = user | any;
+
 function HomePageUser() {
-  const [user, setUser] = useState<user>();
+  const theme = ButtonStyles;
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+  const [searchValues, setSearchValues] = useState("");
+  const [userType, setUserType] = useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(event.target.value);
+    setSearchValues(event.target.value);
+  };
+  const [user, setUser] = useState<any>();
   const [tk, setTk] = useState<any>();
   const navigate = useNavigate();
+  const pages = [
+    { id: 0, text: "Sobre nós" },
+    { id: 1, text: "FAQ" },
+    { id: 2, text: "Contato" },
+  ];
 
-  console.log(tk);
-  //req para pegar o usuario com cpfCnpj === tk
-
-  // const handleLoadUser = useCallback((tk) => {
-  //   Axios.get("localhost:3001/api/users/find", tk)
-  //     .then((res) => {
-  //       console.log(res);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, []);
-
-  const handleLoadUser = () => {
-    // setTk("3232");
-    // console.log("aqui", tk);
-    // let fon: any = "3232";
-    // console.log(fon);
-
-    Axios.get(`http://localhost:3001/api/users/find/${tk}`)
+  useEffect(() => {
+    setTk(localStorage.getItem("token"));
+    Axios.get(
+      `http://localhost:3001/api/users/find/${localStorage.getItem("token")}`
+    )
       .then((res) => {
-        console.log(res);
+        //console.log("req", res.data.user.userType);
+
+        setUser(res.data.user);
+        setUserType(res.data.user.userType);
+        //console.log(user);
       })
       .catch((error) => {
         console.log(error.response);
       });
-  };
-
-  useEffect(() => {
-    setTk(localStorage.getItem("token"));
+    // handleLoadUser();
   }, []);
 
+  //console.log("fora", user);
+  console.log("Tipo do usuario ", userType);
+
   return (
-    <div>
-      <h1>
-        <button onClick={() => handleLoadUser()}>olaaaaaaaaaaaaaaaaaa</button>
-      </h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          width: "100%",
+        }}
+      >
+        <Header
+          pages={pages}
+          isOpenDrawer={isOpenDrawer}
+          setIsOpenDrawer={setIsOpenDrawer}
+        />
+
+        <ResponsiveDrawer
+          pages={pages}
+          isOpenDrawer={isOpenDrawer}
+          setIsOpenDrawer={setIsOpenDrawer}
+        />
+        <Box className="divSearchbarAndItens">
+          <SearchField
+            onChange={handleChange}
+            sx={{ marginTop: { sm: "35px" } }}
+            fullWidth
+            placeholder="Pesquisar evento ou apresentação"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon color="secondary" />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <IconButton>
+                  <ClearIcon color="secondary" />
+                </IconButton>
+              ),
+            }}
+          />
+          <Box>
+            <Itens searchValues={searchValues} />
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
 
