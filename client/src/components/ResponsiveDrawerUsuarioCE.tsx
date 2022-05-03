44381f9a6bd862d90f1f6a2e91e2cdde03683731
rafa <jerhemoso18@gmail.com>
2022-05-03 04:28:48 -0300

    .

--- client/src/components/ResponsiveDrawerUsuarioCE.tsx
+++ client/src/components/ResponsiveDrawerUsuarioCE.tsx
@@ -1,4 +1,4 @@
-import '../styles/Drawer.css';
+import "../styles/Drawer.css";
 import {
   Box,
   Divider,
@@ -6,19 +6,20 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-} from '@mui/material';
-import Drawer from '@mui/material/Drawer';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
-import LoginIcon from '@mui/icons-material/Login';
-import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
-import { ThemeProvider } from '@mui/material/styles';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import MuiStyles from '../styles/MuiStyles';
-import CreateEventsModalDetails from './CreateEventsModalDetails';
-import CreatePalestraModalDetails from './CreatePalestraModalDetails';
-import { User } from '../pages/HomePageUser';
+} from "@mui/material";
+import Drawer from "@mui/material/Drawer";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import StarOutlinedIcon from "@mui/icons-material/StarOutlined";
+import LoginIcon from "@mui/icons-material/Login";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import { ThemeProvider } from "@mui/material/styles";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import MuiStyles from "../styles/MuiStyles";
+import CreateEventsModalDetails from "./CreateEventsModalDetails";
+import CreatePalestraModalDetails from "./CreatePalestraModalDetails";
+import { User } from "../pages/HomePageUser";
+import { EventData } from "./Itens";
 
 const drawerWidth = 240;
 
@@ -27,6 +28,7 @@ interface Props {
   setIsOpenDrawer: React.Dispatch<React.SetStateAction<boolean>>;
   pages: { id: number; text: string }[];
   user: User;
+  events: EventData[];
 }
 
 const ResponsiveDrawerUsuarioCE: React.FC<Props> = ({
@@ -34,49 +36,52 @@ const ResponsiveDrawerUsuarioCE: React.FC<Props> = ({
   setIsOpenDrawer,
   pages,
   user,
+  events,
 }) => {
   const [criarEvento, setCriarEvento] = useState(false);
   const [criarPalestra, setCriarPalestra] = useState(false);
   const theme = MuiStyles;
-  const [openModalDetailsPalestra, setOpenModalDetailsPalestra] = React.useState(false);
+  const [openModalDetailsPalestra, setOpenModalDetailsPalestra] =
+    React.useState(false);
 
   const handleDrawerToggle = () => {
     console.log(isOpenDrawer);
     setIsOpenDrawer(!isOpenDrawer);
     console.log(isOpenDrawer);
   };
-  const [openModalDetailsEvento, setOpenModalDetailsEvento] = React.useState(false);
+  const [openModalDetailsEvento, setOpenModalDetailsEvento] =
+    React.useState(false);
 
   const handleDrawerClick = (id: number) => {
     // setIsOpenModalCreateProduct(true);
     if (id === 1) {
-      console.log('mostrar aqui a tela de criar eventos');
+      console.log("mostrar aqui a tela de criar eventos");
       setCriarEvento(true);
       console.log(criarEvento);
       setOpenModalDetailsEvento(true);
     }
     if (id === 2) {
-      console.log('mostrar aqui a tela de criar palestras');
+      console.log("mostrar aqui a tela de criar palestras");
       setCriarPalestra(true);
       console.log(criarPalestra);
       setOpenModalDetailsPalestra(true);
     }
   };
   const itensDrawer = [
-    { id: 0, text: 'Home', icon: <HomeOutlinedIcon color="secondary" /> },
+    { id: 0, text: "Home", icon: <HomeOutlinedIcon color="secondary" /> },
     {
       id: 1,
-      text: 'Criar evento',
+      text: "Criar evento",
       icon: <StarOutlinedIcon color="secondary" />,
     },
     {
       id: 2,
-      text: 'Criar palestra',
+      text: "Criar palestra",
       icon: <StarOutlinedIcon color="secondary" />,
     },
     {
       id: 3,
-      text: 'Relatórios',
+      text: "Relatórios",
       icon: <ShoppingBagOutlinedIcon color="secondary" />,
     },
   ];
@@ -84,13 +89,13 @@ const ResponsiveDrawerUsuarioCE: React.FC<Props> = ({
   const drawer = (
     <Box>
       <List>
-        <ListItem button sx={{ display: { sm: 'none' } }}>
+        <ListItem button sx={{ display: { sm: "none" } }}>
           <ListItemIcon>
             <LoginIcon color="secondary" />
           </ListItemIcon>
           <Link
-            to={'/LoginAndRegister'}
-            style={{ textDecoration: 'none', color: 'inherit' }}
+            to={"/LoginAndRegister"}
+            style={{ textDecoration: "none", color: "inherit" }}
           >
             <ListItemText primary="Login/Cadastro" />
           </Link>
@@ -107,7 +112,7 @@ const ResponsiveDrawerUsuarioCE: React.FC<Props> = ({
         ))}
       </List>
       <Divider variant="middle" color="#fff" />
-      <Box sx={{ display: { sm: 'none' } }}>
+      <Box sx={{ display: { sm: "none" } }}>
         <List>
           {pages.map((page) => (
             <ListItem
@@ -126,7 +131,7 @@ const ResponsiveDrawerUsuarioCE: React.FC<Props> = ({
 
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: "flex" }}>
         <Box
           component="nav"
           sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
@@ -164,6 +169,7 @@ const ResponsiveDrawerUsuarioCE: React.FC<Props> = ({
           openModalDetails={openModalDetailsPalestra}
           setOpenModalDetails={setOpenModalDetailsPalestra}
           user={user}
+          events={events}
         />
       ) : (
         <></>
--- client/src/pages/HomePageUser.tsx
+++ client/src/pages/HomePageUser.tsx
@@ -1,24 +1,24 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import '../styles/HomePageUser.css';
-import Axios from 'axios';
+import "../styles/HomePageUser.css";
+import Axios from "axios";
 
-import { ThemeProvider } from '@mui/material/styles';
-import { Box } from '@material-ui/core';
-import { IconButton, InputAdornment } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import ClearIcon from '@mui/icons-material/Clear';
-import Header from '../components/HeaderLogged';
+import { ThemeProvider } from "@mui/material/styles";
+import { Box } from "@material-ui/core";
+import { IconButton, InputAdornment } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import ClearIcon from "@mui/icons-material/Clear";
+import Header from "../components/HeaderLogged";
 // import ResponsiveDrawer from "../components/ResponsiveDrawer";
-import ResponsiveDrawer from '../components/ResponsiveDrawer';
-import Itens from '../components/Itens';
-import ButtonStyles from '../styles/MuiStyles';
-import SearchField from '../components/SearchField';
-import ResponsiveDrawerUsuarioComum from '../components/ResponsiveDrawerUsuarioComum';
-
-import ResponsiveDrawerUsuarioPalestrante from '../components/ResponsiveDrawerUsuarioPalestrante';
-import ResponsiveDrawerUsuarioCE from '../components/ResponsiveDrawerUsuarioCE';
+import ResponsiveDrawer from "../components/ResponsiveDrawer";
+import Itens, { EventData } from "../components/Itens";
+import ButtonStyles from "../styles/MuiStyles";
+import SearchField from "../components/SearchField";
+import ResponsiveDrawerUsuarioComum from "../components/ResponsiveDrawerUsuarioComum";
+import api from "../services/api";
+import ResponsiveDrawerUsuarioPalestrante from "../components/ResponsiveDrawerUsuarioPalestrante";
+import ResponsiveDrawerUsuarioCE from "../components/ResponsiveDrawerUsuarioCE";
 
 export interface User {
   name: string;
@@ -27,76 +27,92 @@ export interface User {
   cpfCnpj: string;
   cell: string;
   userType: string;
-  userRegisteredEvents: [{
-    eventId: string,
-    numEventTickets: number,
-    userRegisteredPresentationsId: [{
-      presentationId: string,
-      numPresTickets: number,
-    }]
-  }],
+  userRegisteredEvents: [
+    {
+      eventId: string;
+      numEventTickets: number;
+      userRegisteredPresentationsId: [
+        {
+          presentationId: string;
+          numPresTickets: number;
+        }
+      ];
+    }
+  ];
 }
 
 function HomePageUser() {
+  const [Events, setEvents] = React.useState<EventData[]>([]);
   const theme = ButtonStyles;
   const [isOpenDrawer, setIsOpenDrawer] = useState(false);
-  const [searchValues, setSearchValues] = useState('');
-  const [userType, setUserType] = useState('');
+  const [searchValues, setSearchValues] = useState("");
+  const [userType, setUserType] = useState("");
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     // console.log(event.target.value);
     setSearchValues(event.target.value);
   };
   const [user, setUser] = useState<User>({
-    name: '',
-    email: '',
-    password: '',
-    cpfCnpj: '',
-    cell: '',
-    userType: '',
-    userRegisteredEvents: [{
-      eventId: '',
-      numEventTickets: 0,
-      userRegisteredPresentationsId: [{
-        presentationId: '',
-        numPresTickets: 0,
-      }],
-    }],
+    name: "",
+    email: "",
+    password: "",
+    cpfCnpj: "",
+    cell: "",
+    userType: "",
+    userRegisteredEvents: [
+      {
+        eventId: "",
+        numEventTickets: 0,
+        userRegisteredPresentationsId: [
+          {
+            presentationId: "",
+            numPresTickets: 0,
+          },
+        ],
+      },
+    ],
   });
   const [tk, setTk] = useState<any>();
   const navigate = useNavigate();
   const pages = [
-    { id: 0, text: 'Sobre nós' },
-    { id: 1, text: 'FAQ' },
-    { id: 2, text: 'Contato' },
+    { id: 0, text: "Sobre nós" },
+    { id: 1, text: "FAQ" },
+    { id: 2, text: "Contato" },
   ];
 
   useEffect(() => {
-    setTk(localStorage.getItem('token'));
+    setTk(localStorage.getItem("token"));
     Axios.get(
-      `http://localhost:3001/api/users/find/${localStorage.getItem('token')}`,
+      `http://localhost:3001/api/users/find/${localStorage.getItem("token")}`
     )
       .then((res) => {
         // console.log("req", res.data.user.userType);
 
         setUser(res.data.user);
         setUserType(res.data.user.userType);
+        if (res.data.user.userType === "Usuario Criador de Evento") {
+          console.log("fon");
+          api.get("/events").then((response) => {
+            setEvents(response.data);
+          });
+        }
         // console.log(user);
       })
       .catch((error) => {
-        console.log(error.response);
+        //console.log(error.response);
       });
     // handleLoadUser();
   }, []);
 
   // console.log("fora", user);
-  console.log('Tipo do usuario ', userType);
-  console.log(user);
+  // console.log("Tipo do usuario ", userType);
+  // console.log(user);
+  console.log("AAA", Events);
   return (
     <ThemeProvider theme={theme}>
       <Box
         sx={{
-          width: '100%',
-          position: 'fixed',
+          width: "100%",
+          position: "fixed",
         }}
       >
         <Header
@@ -106,18 +122,19 @@ function HomePageUser() {
           setIsOpenDrawer={setIsOpenDrawer}
         />
 
-        {userType === 'Usuario Comum' ? (
+        {userType === "Usuario Comum" ? (
           <ResponsiveDrawerUsuarioComum
             pages={pages}
             isOpenDrawer={isOpenDrawer}
             setIsOpenDrawer={setIsOpenDrawer}
           />
-        ) : userType === 'Usuario Criador de Evento' ? (
+        ) : userType === "Usuario Criador de Evento" ? (
           <ResponsiveDrawerUsuarioCE
             pages={pages}
             isOpenDrawer={isOpenDrawer}
             setIsOpenDrawer={setIsOpenDrawer}
             user={user}
+            events={Events}
           />
         ) : (
           <ResponsiveDrawerUsuarioPalestrante
@@ -130,7 +147,7 @@ function HomePageUser() {
         <Box className="divSearchbarAndItens">
           <SearchField
             onChange={handleChange}
-            sx={{ marginTop: { sm: '35px' }, mb: '25px' }}
+            sx={{ marginTop: { sm: "35px" }, mb: "25px" }}
             fullWidth
             placeholder="Pesquisar evento ou apresentação"
             InputProps={{
